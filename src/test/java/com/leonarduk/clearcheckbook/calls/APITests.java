b78e25fc748964e12c7bc2e7023951945e822873
leonarduk <leonarduk@d24b3147-cc51-4af9-8f22-8d1ba707a770>
2014-02-07 20:40:13 +0000

    import transactions

---
+++ src/test/java/com/leonarduk/clearcheckbook/calls/APITests.java
@@ -0,0 +1,14 @@
+package com.leonarduk.clearcheckbook.calls;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ AccountCallTest.class, CategoryCallTest.class,
+		LimitCallTest.class, ParsedNameValuePairTest.class,
+		PremiumCallTest.class, ReportCallTest.class, TransactionCallTest.class,
+		UserCallTest.class })
+public class APITests {
+
+}
