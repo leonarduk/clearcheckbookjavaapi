4619a5d3d994689a68cf648302daee9c452f4d39
leonarduk <leonarduk@d24b3147-cc51-4af9-8f22-8d1ba707a770>
2014-02-07 20:16:45 +0000

    import transactions

--- src/main/java/com/leonarduk/clearcheckbook/ClearCheckBookHelper.java
+++ src/main/java/com/leonarduk/clearcheckbook/ClearCheckBookHelper.java
@@ -14,6 +14,7 @@ import com.leonarduk.clearcheckbook.dto.CategoryDataType;
 import com.leonarduk.clearcheckbook.dto.LimitDataType;
 import com.leonarduk.clearcheckbook.dto.ReminderDataType;
 import com.leonarduk.clearcheckbook.dto.TransactionDataType;
+import com.leonarduk.clearcheckbook.file.ClearCheckBookFileHandler;
 import com.leonarduk.clearcheckbook.file.FilePreProcessor;
 
 /**
